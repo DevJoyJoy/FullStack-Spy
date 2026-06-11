e7b44ac4066ff5f3e7ca9864a543716c8b02e6a0
annaguerrra <annaguerracastro@gmail.com>
2026-06-11 16:47:25 -0300

    prototip

--- Backend/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Backend/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523237a1c015e96a6b5c073b8d7b7cd07991e901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8e0f443f362c5fa0edf1442a454565a685a4f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
