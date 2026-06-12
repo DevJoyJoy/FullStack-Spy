ca87bc4303a600cc9f887d3461912f5ddc81000d
DevJoyJoy <joycenascimento2007@gmail.com>
2026-06-12 15:12:09 -0300

    Started on the mai page

---
+++ Code/Frontend/3spys/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../Styles/Components.css'
+
+export function Footer() {
+    return(
+        <div class="footer">
+            <section class="menuItem">
+                <img src="../../public/circle-blank.svg" alt="" />
+
+            </section>
+        </div>
+    )
+}
---
+++ Code/Frontend/3spys/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import '../Styles/Components.css'
+
+export function Header(){
+    return(
+        <div className="header">
+            <img src="../../public/mainIcon.svg" alt="" />
+            <p>ThreeSpies</p>
+        </div>
+    )
+}
