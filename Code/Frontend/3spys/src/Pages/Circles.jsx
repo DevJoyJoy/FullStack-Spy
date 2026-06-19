350caf7c4245a3b9bd1633631bc0dfcd21f41515
pombodivoso <joycenascimento2007@gmail.com>
2026-06-18 21:17:55 -0300

    Group modals done

--- Code/Frontend/3spys/src/Pages/Circles.jsx
+++ Code/Frontend/3spys/src/Pages/Circles.jsx
@@ -1,8 +1,11 @@
 import { Header } from "../Components/Header"
 import { Footer } from "../Components/Footer"
+import { useState } from "react"
 import '../Styles/Circles.css'
 
 export function Circles() {
+    const [createCircleModal, setCreateCircle] = useState(false);
+    
     return(
         <>
         <div className="page">
@@ -16,29 +19,45 @@ export function Circles() {
                 </div>
                 <div className='optionsContainer_'>
 
-                    <div className="option">
+                    <button className="option" onClick={() => setCreateCircle(true)}>
                         <img src="/circle-blank.svg" alt="" />
                         <section>
                             <h1>Criar círculo</h1>
                         </section>
-                    </div>
+                    </button>
 
-                    <div className="option">
+                    <button className="option">
                         <img src="/circle-blank.svg" alt="" />
                         <section>
                             <h1>Visualizar círculo</h1>
                         </section>
-                    </div>
+                    </button>
 
-                    <div className="option">
+                    <button className="option">
                         <img src="/circle-blank.svg" alt="" />
                         <section>
                             <h1>Alterar círculo</h1>
                         </section>
-                    </div>
+                    </button>
                 </div>
             <Footer></Footer>
         </div>
+
+        {createCircleModal && (
+        <div className="modalOverlay">
+          <div className="modalBackdrop" onClick={() => {setCreateCircle(false);}}></div>
+
+            <div className="modalContainer">
+                <h2>Criar círculo</h2>
+                <br />
+                <input className="modalInput" type="text" placeholder="Insira aqui o endereço do círculo:" />
+                <br />
+                <button onClick={() => setCreateCircle(false)} className="modalButton">
+                    Criar
+                </button>
+            </div>
+            </div>
+        )}
         </>
     )
 }
--- Code/Frontend/3spys/src/Pages/Group.jsx
+++ Code/Frontend/3spys/src/Pages/Group.jsx
@@ -1,29 +1,65 @@
 import { Header } from "../Components/Header"
 import { Footer } from "../Components/Footer"
+import { useState } from "react";
 import '../Styles/Group.css'
 
 export function Group() {
+    const [enterGroupModal, setEnterGroup] = useState(false);
+    const [createGroupModal, setCreateGroup] = useState(false);
+
     return(
         <>
         <div className="page">
             <Header></Header>
                 <div className='optionsContainer'>
-                    <div className="option">
-                        <img src="/circle-blank.svg" alt="" />
-                        <section>
-                            <h1>Criar grupo</h1>
-                        </section>
-                    </div>
+                    <button className="option" onClick={() => setCreateGroup(true)}>
+                            <img src="/circle-blank.svg" alt="" />
+                            <section>
+                                <h1>Criar grupo</h1>
+                            </section>
+                    </button>
 
-                    <div className="option">
+                    <button className="option" onClick={() => setEnterGroup(true)}>
                         <img src="/circle-blank.svg" alt="" />
                         <section>
                             <h1>Entrar em grupo</h1>
                         </section>
-                    </div>
+                    </button>
                 </div>
             <Footer></Footer>
         </div>
+
+        {createGroupModal && (
+        <div className="modalOverlay">
+          <div className="modalBackdrop" onClick={() => {setCreateGroup(false);}}></div>
+
+            <div className="modalContainer">
+                <h2>Criar grupo</h2>
+                <br />
+                <input className="modalInput" type="text" placeholder="Insira aqui o nome do grupo:" />
+                <br />
+                <button onClick={() => setCreateGroup(false)} className="modalButton">
+                    Criar
+                </button>
+            </div>
+            </div>
+        )}
+
+        {enterGroupModal && (
+        <div className="modalOverlay">
+          <div className="modalBackdrop" onClick={() => {setEnterGroup(false);}}></div>
+
+            <div className="modalContainer">
+                <h2>Entrar em grupo</h2>
+                <br />
+                <input className="modalInput" type="text" placeholder="Insira aqui o código do grupo:" />
+                <br />
+                <button onClick={() => setEnterGroup(false)} className="modalButton">
+                    Entrar
+                </button>
+            </div>
+            </div>
+        )}
         </>
     )
 }
