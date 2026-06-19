8c9866166b3947cf9244b64dc906ce217516be49
DevJoyJoy <joycenascimento2007@gmail.com>
2026-06-19 16:46:50 -0300

    Endpoints for location done

--- Code/Backend/Api/Controller/LocationController.cs
+++ Code/Backend/Api/Controller/LocationController.cs
@@ -13,11 +13,28 @@ public class LocationController(
 ) : ControllerBase
 {
     [HttpPost]
-    public async Task<Result<CreateLocationResponse>> CreateLocation(
+    public async Task<IActionResult> CreateLocation(
+        [FromBody] CreateLocationPayload payload
+    )
+    {
+        var response = await createLocationUseCase.Do(payload);
+
+        if(!response.IsSuccess)
+            return BadRequest(response);
+        
+        return Ok(response.data);
+    }
+
+    [HttpGet("/{id}")]
+    public async Task<IActionResult> GetLastLocation(
         [FromRoute] int id
     )
     {
-        var response = createLocationUseCase.Do
+        var response = await lastlocationUseCase.Do(id);
+
+        if(!response.IsSuccess)
+            return BadRequest(response);
         
+        return Ok(response.data);
     }
 }
